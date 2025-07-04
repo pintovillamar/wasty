df3b633b74e2ff885d9b25521d5ece2b45cca7e1
jose pinto <jpinto@ulasalle.edu.pe>
2025-07-04 11:39:44 -0500

    Fix import statement in create_app function to correctly reference the app module

--- app/database.py
+++ app/database.py
@@ -18,7 +18,7 @@ ma = Marshmallow()
 
 def create_app():
     
-    # import app.models
+    import app
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
